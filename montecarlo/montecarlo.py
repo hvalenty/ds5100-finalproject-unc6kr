0957a7633df5bbf812e0a8af2a6a3b4693b11edb
hvalenty <hvalenty26@gmail.com>
2024-06-28 11:02:26 -0400

    Upload skeleton of Monte Carlo classes

---
+++ montecarlo/montecarlo.py
@@ -0,0 +1,22 @@
+class Die():
+    '''
+    The Die Class.
+    '''
+    def __init__():
+        pass
+
+
+class Game():
+    '''
+    The Game Class.
+    '''
+    def __init__():
+        pass
+
+
+class Analyzer():
+    '''
+    The Analyzer Class.
+    '''
+    def __init__():
+        pass
