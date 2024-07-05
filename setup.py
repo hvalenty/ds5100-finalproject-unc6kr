464beec1a6a1d500ac866d3860041fba73e84df4
hvalenty <hvalenty26@gmail.com>
2024-07-05 11:46:54 -0400

    Add setup, test, and initalizer files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Monte Carlo Game',
+    version='0.1',
+    description='Package for game monte carlo simulations.',
+    url='https://github.com/hvalenty/ds5100-finalproject-unc6kr',
+    author='H. Valenty',
+    author_email='hvalenty26@gmail.com',
+    license='MIT',
+    packages=['montecarlo']
+)
